7a99cae058ec9d41c52fe8bd9b2686dc9cc76a27
Terence <terenceodhiambosam@gmail.com>
2026-06-08 09:46:04 +0200

    Initial Commit

---
+++ mAJIMAZURI/mAJIMAZURIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mAJIMAZURIApp.swift
+//  mAJIMAZURI
+//
+//  Created by etud on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct mAJIMAZURIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mAJIMAZURITests/mAJIMAZURITests.swift
@@ -0,0 +1,17 @@
+//
+//  mAJIMAZURITests.swift
+//  mAJIMAZURITests
+//
+//  Created by etud on 08/06/2026.
+//
+
+import Testing
+@testable import mAJIMAZURI
+
+struct mAJIMAZURITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
